420b86e6b4fe5534f482353d8699074df4da314f
Vikash Khati <124573409+vikashkhati007@users.noreply.github.com>
2023-07-19 22:56:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,47 @@
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Replace the value below with the Telegram token you receive from @BotFather
 const token = '6394151990:AAGQFLNZ7u3z9BStOVdMGoPeoeFtU4--fY0';
+const bot = new TelegramBot(token);
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+// Create an Express app
+const app = express();
 
-let randomNumber = 0;
-// Matches "/echo [whatever]"
-bot.onText(/\/newemail/, (msg, match) => {
-    // Move the 'num' variable outside the function
-    let num = 0;
-    randomNumber++;
-    
-    const getmessagelist = async () => {
-        let email = `nichacks${randomNumber}@mailto.plus`;
-        let chatId = msg.chat.id;
-        bot.sendMessage(chatId, "YOUR EMAIL -> " + email);
-        
+// Use bodyParser middleware to parse incoming requests
+app.use(bodyParser.json());
 
-        intervalID = setInterval(async () => {
-            let res = await axios.get(`https://tempmail.plus/api/mails?email=${email}`);
-            if (res.data.count > num) {
-                for (let int = 0; int < 1; int++) {
-                    bot.sendMessage(chatId, "MESSAGE RECEIVED: -> " + res.data.mail_list[int].subject);
-                }
-                num = num + 1;
-            }
-        },200);
-
-    }
-    getmessagelist();
-   
-    // Send back the matched "whatever" to the chat
+// Endpoint to receive updates from Telegram
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
-// Listen for any kind of message. There are different kinds of messages.
-bot.on('message', (msg) => {
-    // Send a message to the chat acknowledging receipt of their message
+// Matches "/newemail"
+bot.onText(/\/newemail/, (msg, match) => {
+  let randomNumber = Math.floor(Math.random() * 1000) + 1;
+  let num = 0;
+  let email = `nichacks${randomNumber}@mailto.plus`;
+  let chatId = msg.chat.id;
+  bot.sendMessage(chatId, "YOUR EMAIL -> " + email);
+
+  const intervalID = setInterval(async () => {
+    try {
+      let res = await axios.get(`https://tempmail.plus/api/mails?email=${email}`);
+      if (res.data.count > num) {
+        for (let int = 0; int < 1; int++) {
+          bot.sendMessage(chatId, "MESSAGE RECEIVED: -> " + res.data.mail_list[int].subject);
+        }
+        num = num + 1;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, 200);
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Bot server started on port ${port}`);
 });
